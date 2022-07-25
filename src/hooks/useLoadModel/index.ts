85a56885ff2cf20152b538f8fba5f189645adac4
guhcalm <guhcalm@outlook.com>
2022-07-25 02:04:55 -0400

    main

--- src/hooks/useLoadModel/index.ts
+++ src/hooks/useLoadModel/index.ts
@@ -3,29 +3,29 @@ import { Group, Mesh, MeshStandardMaterial } from "three"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader"
 
-let model
-new GLTFLoader()
-  .setDRACOLoader(
-    new DRACOLoader()
-      .setDecoderPath("/examples/js/libs/draco/")
-      .setDecoderConfig({ type: "js" })
-  )
-  .load("./assets/model/scene.gltf", ({ scene }) => {
-    scene.traverse(object => {
-      if (!(object instanceof Mesh)) return
-      object.castShadow = false
-      object.receiveShadow = false
-      object.material.dispose()
-      object.material = new MeshStandardMaterial({
-        color: "white",
-        roughness: 1,
-        metalness: 0
-      })
-    })
-    model = scene
-  })
 export default () => {
   const [ThreeGraces, setThreeGraces] = useState(new Group())
-  useEffect(() => {}, [])
+  useEffect(() => {
+    new GLTFLoader()
+      .setDRACOLoader(
+        new DRACOLoader()
+          .setDecoderPath("/examples/js/libs/draco/")
+          .setDecoderConfig({ type: "js" })
+      )
+      .load("./assets/model/scene.gltf", ({ scene }) => {
+        scene.traverse(object => {
+          if (!(object instanceof Mesh)) return
+          object.castShadow = false
+          object.receiveShadow = false
+          object.material.dispose()
+          object.material = new MeshStandardMaterial({
+            color: "white",
+            roughness: 1,
+            metalness: 0
+          })
+        })
+        setThreeGraces(scene)
+      })
+  }, [])
   return { model: ThreeGraces }
 }

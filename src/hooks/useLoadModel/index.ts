fdfdbd03f92914ce2047af4b22fcd6e05e22c42a
guhcalm <guhcalm@outlook.com>
2022-07-27 11:48:01 -0400

    main

--- src/hooks/useLoadModel/index.ts
+++ src/hooks/useLoadModel/index.ts
@@ -1,5 +1,5 @@
 import { useEffect } from "react"
-import { Mesh, MeshStandardMaterial } from "three"
+import { Mesh, MeshLambertMaterial } from "three"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader"
 import { useCustomContext } from ".."
@@ -13,21 +13,18 @@ export default () => {
           .setDecoderPath("https://www.gstatic.com/draco/v1/decoders/")
           .setDecoderConfig({ type: "js" })
       )
-      .load("./assets/model/scene.gltf", ({ scene }) => {
+      .load("./assets/model/graces.glb", ({ scene }) => {
         scene.traverse(object => {
           if (!(object instanceof Mesh)) return
           object.matrixAutoUpdate = false
           object.castShadow = false
           object.receiveShadow = false
           object.material.dispose()
-          object.material = new MeshStandardMaterial({
-            color: "white",
-            roughness: 1,
-            metalness: 0
+          object.material = new MeshLambertMaterial({
+            color: "white"
           })
         })
-        scene.scale.set(0.1, 0.1, 0.1)
-        scene.position.set(-3.5, -10.5, 0)
+        scene.position.set(-0.2, -2.23, -1.9)
         dispatch(actions.loadModel(scene))
       })
   }, [])
